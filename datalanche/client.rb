5757618bc384969e033c586141d4b6f2cabb3943
wcao101 <weiqiangcao@gmail.com>
2014-04-08 10:49:01 -0600

    rearrange the https encoding

--- datalanche/client.rb
+++ datalanche/client.rb
@@ -8,7 +8,6 @@ require "uri"
 require "../../datalanche/query.rb"
 #require "../../datalanche/exception"
 
-
 class DLClient
     def initialize(key, secret, host = nil, port = nil, verify_ssl = true)
 
@@ -16,7 +15,6 @@ class DLClient
         @auth_secret = secret
         @url = 'https://api.datalanche.com'
         @verify_ssl = verify_ssl
-
 
         if host != nil
             @url = 'https://' + host
@@ -57,41 +55,44 @@ class DLClient
             raise Exception('query == nil')
         end
         
-        # testing query.params
-        params = {}
-        params = params.merge(q.params)
-
         puts "the params are: #{q.params}"
 
         url = @url
 
-        if params.has_key?('database')
+        if q.params.has_key?('database')
             url = url + '/' + URI.parse(str(params['database']))
-            params.delete('database')
+            q.params.delete('database')
         end
 
-        url = URI.parse(URI.encode(url + '/query'))
-        
+        url = URI.parse(url + '/query')
+
         header = {
                     'Accept-Encoding'=>'gzip',
                     'Content-Type'=>'application/json',
                     'User-Agent'=>'Datalanche Ruby Client'
                 }
-
-        puts "url host is: #{url.host}"
-        puts "url port is: #{url.port}"        
-        puts "verify_ssl is: #{@verify_ssl}"
-        #https = Net::HTTP.new(url.host, url.port)
-        req = Net::HTTP::Post.new(url.request_uri)
+        
+        req = Net::HTTP::Post.new(url.path, header)
         req.basic_auth @auth_key, @auth_secret
+        https = Net::HTTP.new(url.host,url.port)
+        https.use_ssl = true
+        https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        https.ssl_version = "SSLv3"
+        req.body = "#{q.params.to_json}"
+        res = https.request(req)
+        puts "Response #{res.code} #{res.message}: #{res.body}"
 
-        req.set_form_data(params, header)
 
-        resp = Net::HTTP.new(url.host, url.port)
-        resp.use_ssl = true
-        resp.ssl_version="SSLv3"
-        resp.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        response = resp.start {|http| http.request(req) }
+        # req = Net::HTTP::Post.new(url.request_uri)
+
+
+        # req.post_form(JSON.dump(q.params), header)
+
+        # resp = Net::HTTP.new(url.host, url.port)
+        # resp.use_ssl = true
+        # resp.ssl_version = "SSLv3"
+        # resp.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        # response = resp.start {|http| http.request(req)}
 
         ## test the request body
         puts "the request body is: #{response.body}"
