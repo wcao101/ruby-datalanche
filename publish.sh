4c642034bc8ce7bde3be3407fe8e33767c9667d1
wcao <weiqiangcao@gmail.com>
2014-04-24 11:24:17 -0600

    remodified publish.sh for publishing and correct the ruby client version

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 find . -type f -name "*~" -exec rm -f {} \; # remove gedit temp files
+rm -f datalanche*.gem
 gem build datalanche.gemspec
-sudo gem install datalanche*.gem
 gem push datalanche*.gem
